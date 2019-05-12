2817674837cecd1538a872e30606bf01ba5011db
Nofit <33626766+Nofit@users.noreply.github.com>
2019-05-12 14:19:30 +0200

    test tutorial

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract Election{
+    // Store candidate
+    // Read candidate
+    // Constructor
+    string public candidate;
+
+    constructor () public {
+        candidate = "Candidate 1";
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,3 +3,10 @@ var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
 };
+
+var Election = artifacts.require("./Election.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Election);
+};
+
