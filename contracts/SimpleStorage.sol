4630cc849e0d67e06c4f3d43a8e03af928eb7062
Jurian-PC\Jurian <j.vink@gdksoftware.nl>
2019-05-18 21:52:42 +0200

    added weather api

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -5,9 +5,14 @@ contract SimpleStorage {
 
   function set(uint x) public {
     storedData = x;
+    emit HTTPRequest("http://api.openweathermap.org/data/2.5/weather?q=Dordrecht,nl&units=metric&APPID=55e3d06cfe25b54ec349eae880b98d57");
   }
 
   function get() public view returns (uint) {
     return storedData;
   }
+
+  event HTTPRequest(
+        string url
+    );
 }
