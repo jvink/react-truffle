aef500f4e711146b10858e11f5595e6165cbd848
Jurian <j.vink@gdksoftware.nl>
2019-05-22 21:32:27 +0200

    removed unused code

--- client/src/Weather.js
+++ client/src/Weather.js
@@ -1,6 +1,3 @@
-// import getWeb3 from "./utils/getWeb3";
-// import SimpleStorageContract from "./contracts/SimpleStorage.json";
-
 const getWeather = async (city) => {
     const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city},nl&units=metric&APPID=55e3d06cfe25b54ec349eae880b98d57`;
 
@@ -16,22 +13,3 @@ const getWeather = async (city) => {
 }
 
 export default getWeather;
-
-    // const web3 = await getWeb3();
-    // // get contract
-
-    // var bettingArenaContract = web3.eth.contract(SimpleStorageContract.abi);
-
-    // // get the event (check https://github.com/ethereum/wiki/wiki/JavaScript-API#web3ethcontract, 
-    // // Events section, for the parameters)
-    // var event = bettingArenaContract.HTTPRequest({ fromBlock: 0, toBlock: 'latest' });
-    // console.log(event);
-    // //get all events from 0 block to the latest
-    // event.get((error, result) => {
-    //     // handle the result
-    // });
-
-    // //observe new events
-    // event.watch(function (error, result) {
-    //     // handle the result
-    // });
