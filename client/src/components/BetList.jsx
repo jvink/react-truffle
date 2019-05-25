88024fd79496170ee65a8b20fa500fc74ef76e6b
Jurian <j.vink@gdksoftware.nl>
2019-05-25 23:51:41 +0200

    Added list of bets and status

--- client/src/components/BetList.jsx
+++ client/src/components/BetList.jsx
@@ -1,47 +1,55 @@
 import React, { Component } from "react";
 import "../App.css";
+import BetComponent from "./Bet";
 
 
 class App extends Component {
-    state = {stackId:null, bets: null};
+  state = { stackId: null, bets: null };
 
-    componentWillReceiveProps = async (props) => {
-      this.setState({bets: props.bets, drizzle: props.drizzle})
-    }
-  
-    setValue = async () => {
-        const { drizzle, drizzleState} = this.props;
-        const contract = drizzle.contracts.SimpleStorage;
-    // let drizzle know we want to call the `add` method with `value1 and value2`
-        const stackId = await contract.methods["set"].cacheSend(100 ,{
-          from: drizzleState.accounts[0], gas: 2000000
-        });
-        console.log(stackId)
-    // save the `stackId` for later reference
-        this.setState({ stackId });
-      };
-    
-    render() {
-      if (this.state.loading ) {
-        return <div>Loading Drizzle, Web3, Metamask...</div>;
-      }
-
-      const {drizzleState, storageValue} = this.props;
-      const {bets} = this.state;
-  // if it exists, then we display its value
-      return (    
-            <div className="card col-4">
-              <h2>Wallet</h2>
-              <p>The stored value is: {storageValue && storageValue}</p>
-              <h3>Accounts: </h3>
-              <ul>
-                <li>{drizzleState && drizzleState.accounts[0] }</li>
-              </ul>
-              <button type="button" onClick={this.setValue} className="btn btn-secondary mb-4">click here</button>
-              <label>Lijst bets (deze nog fatsoeneren)</label>
-              {bets && bets.map((bet, key) => {console.log(bet); return <div key={key}>{bet.cityId}</div>})}
-            </div>
-      );
-    }
+  componentWillReceiveProps = async (props) => {
+    this.setState({ bets: props.bets, drizzle: props.drizzle })
   }
-  export default App;
+
+  setValue = async () => {
+    const { drizzle, drizzleState } = this.props;
+    const contract = drizzle.contracts.SimpleStorage;
+    // let drizzle know we want to call the `add` method with `value1 and value2`
+    const stackId = await contract.methods["set"].cacheSend(100, {
+      from: drizzleState.accounts[0], gas: 2000000
+    });
+    console.log(stackId)
+    // save the `stackId` for later reference
+    this.setState({ stackId });
+  };
+
+  render() {
+    if (this.state.loading) {
+      return <div>Loading Drizzle, Web3, Metamask...</div>;
+    }
+
+    const { drizzleState, storageValue } = this.props;
+    const { bets } = this.state;
+    // if it exists, then we display its value
+    return (
+      <div className="card col-4">
+        <h2>Wallet</h2>
+        <p>The stored value is: {storageValue && storageValue}</p>
+        <h4>Accounts: </h4>
+        <ul>
+          <li>{drizzleState && drizzleState.accounts[0]}</li>
+        </ul>
+        <button type="button" onClick={this.setValue} className="btn btn-secondary mb-4">click here</button>
+        <h4>Your bets:</h4>
+        {bets && bets.map((bet, i) => {
+          if (bet.made_on > bet.weather_date) {
+            return <BetComponent bet={bet} key={i} type={0}/>;
+          } else {
+            return <BetComponent bet={bet} key={i} type={1}/>;
+          }
+        }
+        )}
+      </div>
+    );
+  }
+}
+export default App;
